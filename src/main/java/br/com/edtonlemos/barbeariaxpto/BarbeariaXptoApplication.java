ad29a0228506df8d3976909cbcbde2d86e6d257d
Edton Lemos <edh.15@hotmail.com>
2021-01-02 12:12:10 -0300

    Primeiro commit. Adicionando o projeto base do spring boot da Barbearia XPTO

---
+++ src/main/java/br/com/edtonlemos/barbeariaxpto/BarbeariaXptoApplication.java
@@ -0,0 +1,13 @@
+package br.com.edtonlemos.barbeariaxpto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaXptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaXptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edtonlemos/barbeariaxpto/BarbeariaXptoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edtonlemos.barbeariaxpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaXptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
